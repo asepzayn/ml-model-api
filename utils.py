a736b3395443ddec1701553ddfa37b036966672b
asepzayn <82562533+asepzayn@users.noreply.github.com>
2023-06-16 09:18:19 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,52 @@
 import numpy as np
+import tensorflow as tf
 
-from PIL import Image
-from io import BytesIO
+# The set of characters accepted in the transcription
+characters = [x for x in "abcdefghijklmnopqrstuvwxyz'?! "]
 
-def load_image_into_numpy_array(data):
-    return np.array(Image.open(BytesIO(data)))
+# Mapping characters to integers
+char_to_num = tf.keras.layers.StringLookup(vocabulary=characters, oov_token="")
+
+# Mapping integers back to original characters
+num_to_char = tf.keras.layers.StringLookup(
+                    vocabulary=char_to_num.get_vocabulary(),
+                    oov_token="",
+                    invert=True)
+
+def decode_batch_predictions(pred):
+    input_len = np.ones(pred.shape[0]) * pred.shape[1]
+    results = tf.keras.backend.ctc_decode(pred, input_length=input_len, greedy=True)[0][0]
+    output_text = []
+    for result in results:
+        result = tf.strings.reduce_join(num_to_char(result)).numpy().decode("UTF-8")
+        output_text.append(result)
+    return output_text
+
+def preprocess_audio(file):
+    # Read audio file
+    audio, _ = tf.audio.decode_wav(file)
+    audio = tf.squeeze(audio, axis=-1)
+    audio = tf.cast(audio, tf.float32)
+
+    # Define preprocessing parameters
+    frame_length = 256
+    frame_step = 160
+    fft_length = 384
+
+    # Get the spectrogram
+    spectrogram = tf.signal.stft(
+        audio, frame_length=frame_length, frame_step=frame_step, fft_length=fft_length
+    )
+
+    # Compute magnitude
+    spectrogram = tf.abs(spectrogram)
+    spectrogram = tf.math.pow(spectrogram, 0.5)
+
+    # Normalization
+    means = tf.math.reduce_mean(spectrogram, axis=1, keepdims=True)
+    stddevs = tf.math.reduce_std(spectrogram, axis=1, keepdims=True)
+    spectrogram = (spectrogram - means) / (stddevs + 1e-10)
+
+    return spectrogram
+
+
